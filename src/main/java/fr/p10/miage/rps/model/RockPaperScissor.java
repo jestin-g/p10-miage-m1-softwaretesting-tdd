acdf2df0a41c54ef83604d39323fb5ddb23f6ce9
jestin-g <jestingabriel@gmail.com>
2020-09-15 16:23:15 +0200

    Enumérations Result et Play

---
+++ src/main/java/fr/p10/miage/rps/model/RockPaperScissor.java
@@ -0,0 +1,10 @@
+package fr.p10.miage.rps.model;
+
+public class RockPaperScissor {
+    public RockPaperScissor() {
+    }
+
+    ResultEnum play(PlayEnum p1, PlayEnum p2) {
+        return ResultEnum.LOOSE;
+    }
+}
