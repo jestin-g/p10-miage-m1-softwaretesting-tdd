82b372391fd88289b7c566aac87ae2aa876130bd
jestin-g <jestingabriel@gmail.com>
2020-09-17 15:47:29 +0200

    tests executés avec les DataProviders

--- src/test/java/fr/p10/miage/rps/model/RockPaperScissorTest.java
+++ src/test/java/fr/p10/miage/rps/model/RockPaperScissorTest.java
@@ -2,6 +2,7 @@ package fr.p10.miage.rps.model;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -21,22 +22,45 @@ public class RockPaperScissorTest {
         rps = null;
     }
 
-    @Parameters({"papier", "pierre"})
-    @Test()
-    public void testWinPlay(String p1, String p2) {
-        assertEquals(rps.play(PlayEnum.valueOf(p1), PlayEnum.valueOf(p2)), ResultEnum.WIN);
+    @Test(dataProvider="getDataPlayWin")
+    public void testWinPlay(PlayEnum p1, PlayEnum p2) {
+        assertEquals(rps.play(p1, p2), ResultEnum.WIN);
     }
 
-    @Parameters({"pierre", "pierre"})
-    @Test()
-    public void testTiePlay(String p1, String p2) {
-        assertEquals(rps.play(PlayEnum.valueOf(p1), PlayEnum.valueOf(p2)), ResultEnum.TIE);
+    @Test(dataProvider="getDataPlayTie")
+    public void testTiePlay(PlayEnum p1, PlayEnum p2) {
+        assertEquals(rps.play(p1, p2), ResultEnum.TIE);
     }
 
-    @Parameters({"ciseaux", "pierre"})
-    @Test()
-    public void testLoosePlay(String p1, String p2) {
-        assertEquals(rps.play(PlayEnum.valueOf(p1), PlayEnum.valueOf(p2)), ResultEnum.LOOSE);
+    @Test(dataProvider="getDataPlayLoose")
+    public void testLoosePlay(PlayEnum p1, PlayEnum p2) {
+        assertEquals(rps.play(p1, p2), ResultEnum.LOOSE);
     }
 
+    @DataProvider
+    public Object[][] getDataPlayWin() {
+        return new Object[][]{
+                {PlayEnum.PAPER, PlayEnum.ROCK},
+                {PlayEnum.ROCK, PlayEnum.SCISSORS},
+                {PlayEnum.SCISSORS, PlayEnum.PAPER}
+        };
+    }
+
+    @DataProvider
+    public Object[][] getDataPlayTie() {
+        return new Object[][]{
+                {PlayEnum.PAPER, PlayEnum.PAPER},
+                {PlayEnum.ROCK, PlayEnum.ROCK},
+                {PlayEnum.SCISSORS, PlayEnum.SCISSORS}
+        };
+    }
+
+    @DataProvider
+    public Object[][] getDataPlayLoose() {
+        return new Object[][]{
+                {PlayEnum.PAPER, PlayEnum.SCISSORS},
+                {PlayEnum.ROCK, PlayEnum.PAPER},
+                {PlayEnum.SCISSORS, PlayEnum.ROCK}
+        };
+    }
 }
