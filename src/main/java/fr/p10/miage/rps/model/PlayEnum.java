620395c8f2bf7dfc7555a00fc8753c6f2a3b5fec
jestin-g <jestingabriel@gmail.com>
2020-09-17 16:20:08 +0200

    ajout de la méthode randomPlay() qui retourne un un coup aléatoire

--- src/main/java/fr/p10/miage/rps/model/PlayEnum.java
+++ src/main/java/fr/p10/miage/rps/model/PlayEnum.java
@@ -1,7 +1,18 @@
 package fr.p10.miage.rps.model;
 
+import java.util.List;
+import java.util.Random;
+
 public enum PlayEnum {
     ROCK,
     PAPER,
-    SCISSORS
+    SCISSORS;
+
+    private static final List<PlayEnum> VALUES = List.of(values());
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static PlayEnum randomPlay() {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
 }
