ac8a7bc7e6869ead49211f1b6374697d4b017611
jestin-g <jestingabriel@gmail.com>
2020-09-18 14:39:42 +0200

    game() passe les tests pour WIN et TIE

--- src/main/java/fr/p10/miage/rps/model/RockPaperScissor.java
+++ src/main/java/fr/p10/miage/rps/model/RockPaperScissor.java
@@ -20,7 +20,25 @@ public class RockPaperScissor {
         return ResultEnum.LOOSE;
     }
 
-    ResultEnum play(Player p1, Player p2) {
-        return ResultEnum.LOOSE;
+    ResultEnum game(Player p1, Player p2) {
+        PlayEnum moveP1 = p1.getNextMove();
+        PlayEnum moveP2 = p2.getNextMove();
+        while ((moveP1 != null) && (moveP2 != null)) {
+            ResultEnum r = play(moveP1, moveP2);
+            if (r == ResultEnum.WIN) {
+                p1.setScore(p1.getScore() + 1);
+            } else if (r == ResultEnum.LOOSE) {
+                p2.setScore(p2.getScore() + 1);
+            } else {
+                p1.setScore(p1.getScore() + 1);
+                p2.setScore(p2.getScore() + 1);
+            }
+            moveP1 = p1.getNextMove();
+            moveP2 = p2.getNextMove();
+        }
+        if (p1.getScore() == p2.getScore()) {
+            return ResultEnum.TIE;
+        }
+        return ResultEnum.WIN;
     }
 }
